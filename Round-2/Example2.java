47dc460cef6de7d6881384a28f312f32b7b1c3af
Brijesh Jagad <210540107511@darshan.ac.in>
2022-05-01 12:39:22 +0530

    V1.0

---
+++ Round-2/Example2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Example2 {
+    public static void main(String[] args){
+        String original, rev = null;
+        boolean comp;
+        Scanner scanner = new Scanner(System.in);
+        original = scanner.nextLine();
+        System.out.println(original);
+        comp = original.equals(rev);
+        System.out.print(comp);
+    }
+}
